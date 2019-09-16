96467eafe0d6e1f1fce2fd5054c91a9fe8c4ae00
Tami Reichold <reichold.tamara@gmail.com>
2019-09-16 22:25:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,12 +51,17 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
+# def runner(card_total)
+#   welcome
+#   initial_round
+#   hit? && display_card_total
+#   until card_total > 21
+#     end_game(card_total)
+#   end
+# end
+
 def runner(card_total)
   welcome
   initial_round
-  hit? && display_card_total
-  until card_total > 21
-    end_game(card_total)
-  end
-end
+  
     
