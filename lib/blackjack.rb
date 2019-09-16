2759003ccbc72f037bc5ae9bb948d4aac8438f9b
Tami Reichold <reichold.tamara@gmail.com>
2019-09-16 22:12:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,64 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(sum)
+  puts "Your cards add up to #{sum}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  first_card = deal_card
+  second_card = deal_card
+  sum = first_card + second_card
+  display_card_total(sum)
+  return sum
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  get_user_input
+  new_card = 
+    if get_user_input == 'h'
+      new_card << deal_card()
+      current_total = card_total + new_card
+    elsif get_user_input == 's'
+    else 
+      invalid_command
+    end
+  return current_total
+end
+
+def hit?(card_total)
+  prompt_user
+  answer = get_user_input
+  if answer == 'h'
+    card_total += deal_card
+  elsif answer == 's'
+    card_total
+  else
+    invalid_command
+    hit?(card_total)
+   end
 end
 
 def invalid_command
-  # code invalid_command here
+puts "Please enter a valid command"
 end
 
 #####################################################
