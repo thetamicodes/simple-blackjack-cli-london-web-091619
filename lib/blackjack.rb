da86d2d25f9b19ad692d3f9767e8aa846a5cacbb
Tami Reichold <reichold.tamara@gmail.com>
2019-09-16 22:37:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,9 +63,10 @@ end
 def runner(card_total = 0)
   welcome
   initial_round
-  hit? && display_card_total until card_total == 22
-    
-    
+  hit? until card_total == 22
+    puts display_card_total
+    card_total += 1 
+  end
 end 
 
 i = 0
